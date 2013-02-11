a84111e0ec953e3d3baa9889c9b6b84b14f90d6c
liuyug <liuyug@yahoo.cn>
2013-02-11 15:53:12 +0800

    Fix import to relative path Add setup.py, so it may be pack and distribute

---
+++ setup.py
@@ -0,0 +1,28 @@
+#1/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description=f.read()
+
+setup(name='netdisk',
+        version='1.0',
+        author='Yugang LIU',
+        author_email='liuyug@gmail.com',
+        url='https://github.com/liuyug/netdisk',
+        license='GPLv3',
+        description='Command Line Tool for Network Disk',
+        long_description=long_description,
+        platforms=['noarch'],
+        packages=[
+            'netdisk',
+            'netdisk.dropbox',
+            'netdisk.kuaipan',
+            'netdisk.kuaipan.oauth',
+            'netdisk.kuaipan.poster',
+            ],
+        package_dir={'netdisk': 'netdisk'},
+        #package_data={'': ['README.rst',]},
+        #data_files=[('share/netdisk',['README.rst')],
+        scripts=['netdisk_cli'],
+        )
